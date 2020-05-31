153ac6283599fc6f5d1c82ea2c6e03dd68fa3d53
Renato <renatomarquesteles@gmail.com>
2020-05-31 02:00:30 -0300

    resolucao do desafio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Contact extends React.Component {
   render() {
-    return (<div />);
+    return (
+      <article className="contact" data-testid="contact">
+        <span className="contact__avatar">
+          <img src={this.props.data.avatar} alt="Avatar" />
+        </span>
+        <span className="contact__data">{this.props.data.name}</span>
+        <span className="contact__data">{this.props.data.phone}</span>
+        <span className="contact__data">{this.props.data.country}</span>
+        <span className="contact__data">{this.props.data.admissionDate}</span>
+        <span className="contact__data">{this.props.data.company}</span>
+        <span className="contact__data">{this.props.data.department}</span>
+      </article>
+    );
   }
 }
 
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React from 'react';
+
+import Contact from './Contact';
 
 class Contacts extends React.Component {
-	render() {
-		return (<div />);
-	}
+  constructor(props) {
+    super(props);
+
+    this.state = { contacts: [] };
+  }
+
+  componentDidMount() {
+    fetch('https://5e82ac6c78337f00160ae496.mockapi.io/api/v1/contacts')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => this.setState({ contacts: data }));
+  }
+
+  render() {
+    return (
+      <div className="container" data-testid="contacts">
+        <section className="contacts">
+          <Contact
+            data={{
+              avatar: null,
+              name: 'Nome',
+              phone: 'Telefone',
+              country: 'País',
+              admissionDate: 'Admissão',
+              company: 'Empresa',
+              department: 'Departamento',
+            }}
+          />
+          {this.state.contacts.map((contact) => (
+            <Contact
+              key={contact.id}
+              data={{
+                avatar: contact.avatar,
+                name: contact.name,
+                phone: contact.phone,
+                country: contact.country,
+                admissionDate: contact.admissionDate,
+                company: contact.company,
+                department: contact.department,
+              }}
+            />
+          ))}
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Contacts;
